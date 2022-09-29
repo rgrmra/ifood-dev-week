cf8046536a0ddec8ff6d02ea759f41e91365a924
rgrmra <rgrmra@live.com>
2022-09-29 17:48:37 -0300

    First Commit

---
+++ src/main/java/br/com/rgrmra/ifoodDevweek/IfoodDevWeekApplication.java
@@ -0,0 +1,13 @@
+package br.com.rgrmra.ifoodDevweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rgrmra/ifoodDevweek/IfoodDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rgrmra.ifoodDevweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
